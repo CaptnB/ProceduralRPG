fe60398b5af7b445a3b62240473f7f41a27a8fec
Guillaume <Guillaume@10.82.46.90>
2015-05-27 22:00:06 -0400

    Nessing around with UI

--- core/src/com/captnb/proceduralrpg/screens/NewGameScreen.java
+++ core/src/com/captnb/proceduralrpg/screens/NewGameScreen.java
@@ -5,76 +5,102 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class NewGameScreen extends GameScreen
 {
-    Sound sound;
-    Music music;
-    Boolean init;
+    private Sound sound;
+    private Music music;
+    private Stage stage;
 
     public NewGameScreen(Game game, AssetManager manager, SpriteBatch batch)
     {
         super(game, manager, batch);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void show()
     {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        
         sound = Gdx.audio.newSound(Gdx.files.internal("sounds/test_sound.mp3"));
-        sound.play();
         music = Gdx.audio.newMusic(Gdx.files.internal("music/test_music.mp3"));
         music.setLooping(true);
-        music.play();
+        
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        
+        TextButtonStyle style = new TextButtonStyle();
+        style.font = assets.get("text1_30.ttf", BitmapFont.class);
+        
+        TextButton soundBtn = new TextButton("Sound", style);
+        soundBtn.addListener(new ClickListener()
+            {
+                @Override
+                public void clicked (InputEvent event, float x, float y)
+                {
+                    sound.play();
+                }
+            }
+        );
+
+        TextButton musicBtn = new TextButton("Music", style);
+        musicBtn.addListener(new ClickListener()
+            {
+                @Override
+                public void clicked (InputEvent event, float x, float y)
+                {
+                    if(music.isPlaying())
+                    {
+                        music.pause();
+                    }
+                    else
+                    {
+                        music.play();
+                    }
+                }
+            }
+        );
+
+        table.add(soundBtn).align(Align.topRight).pad(10).expand();
+        table.add(musicBtn).align(Align.topRight).pad(10);
+        table.row().expand();
+        //table.setDebug(true);
     }
 
     @Override
     public void render(float delta)
     {
-        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        TextureAtlas characters = assets.get("images/characters/characters.pack", TextureAtlas.class);
-        BitmapFont font = assets.get("text1_20.ttf", BitmapFont.class);
-        
-        int i = 0;
-        for(AtlasRegion character : characters.getRegions())
-        {
-            int colorI = i % 4;
-            switch(colorI)
-            {
-            case 0:
-                batch.setColor(Color.WHITE);
-                break;
-            case 1:
-                batch.setColor(Color.RED);
-                break;
-            case 2:
-                batch.setColor(Color.GREEN);
-                break;
-            case 3:
-                batch.setColor(Color.BLUE);
-                break;
-            default:
-                batch.setColor(Color.WHITE);
-                break;
-            }
-            int row = ((i % 7) * 225) + 25;
-            int col = ((i / 7) * 220) + 20;
-            batch.draw(character, row, col, 200, 200);
-            
-            font.draw(batch, character.name, row, col - 5 );
-
-            i += 1;
-        }
-        batch.end();
+        stage.act(delta);
+        stage.draw();
+//
+//        batch.begin();
+//        batch.end();
+    }
+    
+    @Override
+    public void resize(int width, int height)
+    {
+        stage.getViewport().update(width, height);
     }
 
+    @Override
+    public void dispose()
+    {
+        stage.dispose();
+    }
 }
